3e470b7b80e05c284234516af2faa5f13261c4d7
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2017-03-28 12:12:50 -0400

    adding root for a the user post view

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   post '/login' => 'devise/sessions#create'
 
+  get 'user_post/:id' => 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
